3902a710ad50a1a2c9c0ae49be081a81ae4ec5bf
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-04 19:48:11 -0300

    alt: added command functions

--- bot/commands.js
+++ bot/commands.js
@@ -8,7 +8,7 @@ const { queryOptions } = require('../api/query-options');
 const { querySearch } = require('../api/query-search');
 
 // Import the bot updates request from mongodb
-const { botUpdate } = require('./bot-updates')
+const { userUpdates } = require('./bot-updates')
 
 // Import the user id request to mongodb
 const { getUserId } = require('./get-userId');
@@ -17,7 +17,7 @@ const { getUserId } = require('./get-userId');
 bot.onText(/\/start/, function startCommand(msg) {
     const chatId = msg.chat.id
     const message = "*Welcome to the Wallpaper Discover Bot.*\nYou can easily discover millions of wallpapers from unsplash for free and using this bot. Enjoy! :)"
-    getUserId(chatId)
+    getUserId(msg, chatId)
     bot.sendMessage(chatId, message, {parse_mode: 'markdown'})
 })
 
@@ -55,6 +55,12 @@ bot.onText(/\/query-search/, function querySearchCommand(msg) {
 // Create a /update command
 bot.onText(/\/update/, function updateCommand(msg) {
     const chatId = msg.chat.id
+    const adminId = process.env.ADMIN_ID
+    if (adminId == chatId) {
+        userUpdates(bot, chatId);
+    } else {
+        bot.sendMessage(chatId, "*You don't have permission to use this command!*")
+    }
     chatUpdate(bot, chatId);
 })
 
