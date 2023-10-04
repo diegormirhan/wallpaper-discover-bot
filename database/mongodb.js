2d1f88756bc8a94b337c7e9c3e9556ee002b4863
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-04 19:46:59 -0300

    new: declared mongodb instance

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,1 +1,20 @@
 require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+
+// Getting the database url
+const url = process.env.DATABASE_URL 
+
+// Connecting to the database
+const client = new MongoClient(url);
+
+// Connecting to the "chats" databse
+const chatsDatabase = client.db('chats')
+
+// Connecting to the "telegram chats" collection
+const telegramChatsCol = chatsDatabase.collection('telegram-chats')
+
+module.exports = {
+    client,
+    chatsDatabase,
+    telegramChatsCol
+}
