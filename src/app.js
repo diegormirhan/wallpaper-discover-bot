2c600f08441cb4ae78b3f8794a62fef904f1cf08
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-17 10:32:35 -0300

    alt: changed api to wallhaven

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 require('dotenv').config()
 const commands = require('../bot/commands');
-const unsplash = require('../api/unsplash');
 const telegram = require('../bot/telegram');
+console.log('Bot is running...');
