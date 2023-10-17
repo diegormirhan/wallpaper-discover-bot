1e8f58f03212a48791bead35a1b5433523eb289b
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-17 10:33:13 -0300

    feat: get landscape wallpapers

---
+++ api/landscape-random.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const axios = require('axios')
+
+const token = process.env.WALLHAVEN_TOKEN
+const apiUrl = 'https://wallhaven.cc/api/v1/search'
+
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Version': 'v1',
+        'apikey': token
+    },
+    params: {
+        'resolutions': '1920x1080',
+        'sorting': 'random'
+    }
+};
+
+const landscapeRandomReq = async (bot, chatId) => {
+    const response = await axios.request(apiUrl, options);
+    const jsonResponse = Object.values(response.data.data).slice(0, 5);
+    const medias = []
+    jsonResponse.forEach(media => {
+        medias.push({
+            type: 'photo',
+            media: media.path
+        })
+    })
+    bot.sendMediaGroup(chatId, medias)
+}
+
+
+module.exports = {
+    landscapeRandomReq
+}
