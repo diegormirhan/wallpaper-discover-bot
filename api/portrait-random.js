128d5cd9ec2bf7e45060ee60b0d027215ea64f76
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-17 10:33:32 -0300

    feat: get portrait wallpapers

---
+++ api/portrait-random.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const axios = require('axios');
+
+const token = process.env.WALLHAVEN_TOKEN
+const apiUrl = 'https://wallhaven.cc/api/v1/search'
+
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Version': 'v1',
+        'apikey': token
+    },
+    params: {
+        'resolutions': '1080x1920',
+        'sorting': 'random'
+    }
+};
+
+const portraitRandomReq = async (bot, chatId) => {
+    const response = await axios.request(apiUrl, options);
+    const jsonResponse = Object.values(response.data.data).slice(0, 5)
+    const medias = []
+    jsonResponse.forEach(media => {
+        medias.push({
+            type: 'photo',
+            media: media.path
+        })
+    })
+    bot.sendMediaGroup(chatId, medias)
+}
+
+module.exports = {
+    portraitRandomReq
+}
