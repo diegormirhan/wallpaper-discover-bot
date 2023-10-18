05a38d3b31698f3a6ded3b635247e02e918a4d1d
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-18 19:26:17 -0300

    alt: refactored code

--- bot/commands.js
+++ bot/commands.js
@@ -13,50 +13,88 @@ const { userUpdates } = require('./bot-updates')
 // Import the user id request to mongodb
 const { getUserId } = require('./get-userId');
 
-// Create a /start command
-bot.onText(/\/start/, function startCommand(msg) {
+// Command Handlers
+const commandHandlers = {
+    '/landscape_random': landscapeRandomReq,
+    '/portrait_random': portraitRandomReq,
+    '/query_option': queryOptions,
+    '/query_search': querySearch,
+};
+
+// Create the commands
+bot.onText(/\/start/, startCommand)
+bot.onText(/\/help/, helpCommand)
+bot.onText(/\/portrait_random/, portraitRandomCommand)
+bot.onText(/\/landscape_random/, landscapeRandomCommand)
+bot.onText(/\/query_option/, queryOptionsCommand)
+bot.onText(/\/query_search/, querySearchCommand)
+bot.onText(/\/update/, updateCommand)
+
+
+// Functions to create commands
+
+// --- /start command ---
+function startCommand(msg) {
     const chatId = msg.chat.id
     const message = "*Welcome to the Wallpaper Discover Bot.*\nYou can easily discover millions of wallpapers from unsplash for free and using this bot. Enjoy! :)"
     getUserId(msg, chatId)
-    bot.sendMessage(chatId, message, {parse_mode: 'markdown'})
-})
+    const options = {
+        parse_mode: 'markdown',
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: 'Landscape Random', callback_data: '/landscape_random' },
+                    { text: 'Portrait Random', callback_data: '/portrait_random' }
+                ],
+                [
+                    { text: 'Query Options', callback_data: '/query_option' },
+                    { text: 'Query Search', callback_data: '/query_search' }
+                ],
+                [
+                    { text: 'Help', callback_data: '/help' }
+                ]
+            ]
+        }
+    }
+    bot.sendMessage(chatId, message, options)
+}
 
-// Create a /help command
-bot.onText(/\/help/, function helpCommand(msg) {
+// --- /help command ---
+function helpCommand(msg) {
     const chatId = msg.chat.id
     const message = "Under development..."
-    bot.sendMessage(chatId, message, {parse_mode: 'Markdown'})
-})
+    bot.sendMessage(chatId, message, { parse_mode: 'Markdown' })
+}
 
-// Create a /portrait-random command
-bot.onText(/\/portrait_random/, function portraitRandomCommand(msg) {
+// --- /portrait_random command ---
+function portraitRandomCommand(msg) {
     const chatId = msg.chat.id
     portraitRandomReq(bot, chatId)
-})
+}
 
-// Create a /landscape-random command
-bot.onText(/\/landscape_random/, function landscapeRandomCommand(msg) {
+// --- /lascape_random command ---
+function landscapeRandomCommand(msg) {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, 'Under development...')
     landscapeRandomReq(bot, chatId)
-})
+}
 
-// Create a /query-options command
-bot.onText(/\/query_option/, function queryOptionsCommand(msg) {
+// --- /query_options command ---
+function queryOptionsCommand(msg) {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, 'Under development...')
     // queryOptions(bot, chatId)
-})
+}
 
-// Create a /query-search command
-bot.onText(/\/query_search/, function querySearchCommand(msg) {
+// --- /query_search command ---
+function querySearchCommand(msg) {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, 'Under development...')
-    // querySearch(bot, chatId)
-})
+    querySearch(bot, chatId)
+}
 
-// Create a /update command
-bot.onText(/\/update/, function updateCommand(msg) {
+// --- /update command ---
+function updateCommand(msg) {
     const chatId = msg.chat.id
     const adminId = process.env.ADMIN_ID
     if (adminId == chatId) {
@@ -65,5 +103,17 @@ bot.onText(/\/update/, function updateCommand(msg) {
         bot.sendMessage(chatId, "*You don't have permission to use this command!*")
     }
     chatUpdate(bot, chatId);
-})
+}
 
+
+// Callback function for the update events
+bot.on('callback_query', (callbackQuery) => {
+    const msg = callbackQuery.message;
+    const data = callbackQuery.data;
+    bot.answerCallbackQuery(callbackQuery.id);
+    if(commandHandlers[data]) {
+        commandHandlers[data](bot, msg.chat.id);
+    } else if (data === '/help') {
+        helpCommand(msg);
+    }
+});
