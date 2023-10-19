e54c3dff90790636c7c6aee7040483b6b487233b
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-19 19:37:25 -0300

    alt: implementing command

--- api/query-options.js
+++ api/query-options.js
@@ -4,9 +4,23 @@ const axios = require('axios');
 const token = process.env.WALLHAVEN_TOKEN;
 const apiUrl = 'https://wallhaven.cc/api/v1/search'
 
-const options = {
-    method: 'GET',
-    headers: {
-        
+
+const apiRequest = async (orientation, opts) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Version': 'v1',
+            'apikey': token
+        },
+        params: {
+            'sorting': 'random',
+            'resolutions': orientation,
+            'q': opts
+        }
     }
+}
+
+const queryOptions = async (bot, chatId) => {
+    
 }
