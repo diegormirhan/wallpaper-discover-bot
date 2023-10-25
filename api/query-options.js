19f60c0d41e36c5b4550803a214f0dd4789628f4
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-24 21:10:26 -0300

    Working...

--- api/query-options.js
+++ api/query-options.js
@@ -19,8 +19,53 @@ const apiRequest = async (orientation, opts) => {
             'q': opts
         }
     }
+
+    const response = await axios.request(apiUrl, options)
+    return Object.values(response.data.data).slice(0, 5)
 }
 
 const queryOptions = async (bot, chatId) => {
-    
+    try {
+        const orientationOptions = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'Landscape', callback_data: '1920x1080'},
+                        {text: 'Portrait', callback_data: '1080x1920'}
+                    ]
+                ]
+            }
+        }
+
+        const sortingOptions = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'Relevance', callback_data: 'relevance'},
+                        {text: 'Date Added', callback_data: 'dateAdded'}
+                    ],
+                    [
+                        {text: 'Random', callback_data: 'random'},
+                        {text: 'Toplist', callback_data: 'toplist'}
+                    ]
+                ]
+            }
+        }
+
+        const categories = {
+            parse_mode: 'Markdown',
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {text: 'Nature', callback_data: 'nature'},
+                        {text: 'Artwork', callback_data: ''}
+                    ]
+                ]
+            }
+        }
+    } catch (error) {
+        console.log('Error:', error)
+    }
 }
