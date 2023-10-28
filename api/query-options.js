5ae46ee88fd60fc89c990ede7de91bd4dbdd46b3
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-10-28 17:46:03 -0300

    feat: implemented query options command

--- api/query-options.js
+++ api/query-options.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 
 const token = process.env.WALLHAVEN_TOKEN;
 const apiUrl = 'https://wallhaven.cc/api/v1/search'
-
-
-const apiRequest = async (orientation, opts) => {
+let currentCallbackListener = null;
+let sorting, resolution, query = null
+const apiRequest = async (sorting, resolution, query) => {
     const options = {
         method: 'GET',
         headers: {
@@ -14,9 +14,9 @@ const apiRequest = async (orientation, opts) => {
             'apikey': token
         },
         params: {
-            'sorting': 'random',
-            'resolutions': orientation,
-            'q': opts
+            'sorting': sorting,
+            'resolutions': resolution,
+            'q': query
         }
     }
 
@@ -31,41 +31,98 @@ const queryOptions = async (bot, chatId) => {
             reply_markup: {
                 inline_keyboard: [
                     [
-                        {text: 'Landscape', callback_data: '1920x1080'},
-                        {text: 'Portrait', callback_data: '1080x1920'}
+                        { text: 'Landscape', callback_data: '1920x1080' },
+                        { text: 'Portrait', callback_data: '1080x1920' }
                     ]
                 ]
             }
         }
 
-        const sortingOptions = {
-            parse_mode: 'Markdown',
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {text: 'Relevance', callback_data: 'relevance'},
-                        {text: 'Date Added', callback_data: 'dateAdded'}
-                    ],
-                    [
-                        {text: 'Random', callback_data: 'random'},
-                        {text: 'Toplist', callback_data: 'toplist'}
-                    ]
-                ]
-            }
+        bot.sendMessage(chatId, '*Choose the orientation below:*', orientationOptions)
+
+        if (currentCallbackListener) {
+            bot.removeListener('callback_query', currentCallbackListener);
         }
 
-        const categories = {
-            parse_mode: 'Markdown',
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {text: 'Nature', callback_data: 'nature'},
-                        {text: 'Artwork', callback_data: ''}
-                    ]
-                ]
+        currentCallbackListener = (callbackQuery) => {
+            const data = callbackQuery.data;
+
+            if (data === '1920x1080' || data === '1080x1920') {
+                resolution = data;
+                const sortingOptions = {
+                    chat_id: callbackQuery.message.chat.id,
+                    message_id: callbackQuery.message.message_id,
+                    parse_mode: 'Markdown',
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                { text: 'Relevance', callback_data: 'relevance' },
+                                { text: 'Date Added', callback_data: 'dateAdded' }
+                            ],
+                            [
+                                { text: 'Random', callback_data: 'random' },
+                                { text: 'Toplist', callback_data: 'toplist' }
+                            ]
+                        ]
+                    }
+                }
+                bot.editMessageText('*Choose the sorting below:*', sortingOptions);
+            } else if (data === 'relevance' || data === 'dateAdded' || data === 'random' || data === 'toplist') {
+                sorting = data
+                const categories = {
+                    chat_id: callbackQuery.message.chat.id,
+                    message_id: callbackQuery.message.message_id,
+                    parse_mode: 'Markdown',
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                { text: 'Nature', callback_data: 'nature' },
+                                { text: 'Artwork', callback_data: 'artwork' }
+                            ],
+                            [
+                                { text: 'Abstract', callback_data: 'abstract' },
+                                { text: 'Futuristic', callback_data: 'futuristic' }
+                            ],
+                            [
+                                { text: 'City', callback_data: 'city' },
+                                { text: 'Sky', callback_data: 'sky' }
+                            ],
+                            [
+                                { text: 'Anime', callback_data: 'anime' },
+                                { text: 'Cars', callback_data: 'car' }
+                            ]
+                        ]
+                    }
+                }
+                bot.editMessageText('*Choose the category below:*', categories)
+
+            } else {
+                query = data
+                userResponse(sorting, resolution, query)
             }
+        }
+        bot.on('callback_query', currentCallbackListener)
+
+        const userResponse = async (sorting, resolution, query) => {
+            const jsonResponse = await apiRequest(sorting, resolution, query)
+            if (jsonResponse.length === 0) {
+                bot.sendMessage(chatId, 'There was an error trying to send the message. Try again later...')
+            } else {
+            const medias = []
+            jsonResponse.forEach(media => {
+                medias.push({
+                    type: 'photo',
+                    media: media.path
+                })
+            })
+            bot.sendMediaGroup(chatId, medias)
+        }
         }
     } catch (error) {
-        console.log('Error:', error)
+        bot.sendMessage(chatId, 'There was an error trying to send the message')
     }
+}
+
+module.exports = {
+    queryOptions
 }
