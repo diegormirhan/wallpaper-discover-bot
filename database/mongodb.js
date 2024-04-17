33523c24462fc2fd445ab4db198ee1c49a3c268e
Diego Mirhan <mirhan.diego@gmail.com>
2024-04-17 19:38:24 -0300

    update

--- database/mongodb.js
+++ database/mongodb.js
@@ -7,7 +7,7 @@ const url = process.env.DATABASE_URL
 // Connecting to the database
 const client = new MongoClient(url);
 
-// Connecting to the "chats" databse
+// Connecting to the "chats" databse.
 const chatsDatabase = client.db('chats')
 
 // Connecting to the "telegram chats" collection
